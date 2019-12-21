2990be582711052a0014e9e783644e91571452a2
yemingzhu <140465097@qq.com>
2019-12-21 19:20:58 +0800

    添加设备接口

--- src/api/api.js
+++ src/api/api.js
@@ -6,42 +6,51 @@ const httpPost = (url, data) => {
       method: 'post',
       header: {
         'content-type': 'application/x-www-form-urlencoded',
-        'token': wx.getStorageSync("token")
+        token: wx.getStorageSync('token')
       },
       data: data,
-      success: function (res) {
-        if (res.code == 300) {
+      success: function(res) {
+        if (res.code == 0) {
+          //TODO 回传所有数据
+          resolve(res.data);
+        } else if (res.code == 300) {
           //TODO token失效
+          //调用/app/public/getToken 获取新的token 同时判断是否需要重新获取用户信息以及手机号
+          console.log('token失效，需要重新调用/app/public/getToken');
+        } else {
+          console.log(res);
+          reject(res.msg);
         }
-        //TODO 回传所有数据
-        resolve(res.data);
       },
-      fail: function (err) {
-        reject(err)
+      fail: function(err) {
+        reject(err);
       }
-    })
-  })
-}
+    });
+  });
+};
 
 const showModal = (title, content) => {
   return new Promise((resolve, reject) => {
     wx.showModal({
       title: title,
       content: content,
-      success: function (res) {
+      success: function(res) {
         resolve(res);
       },
-      fail: function (err) {
-        reject(err)
+      fail: function(err) {
+        reject(err);
       }
-    })
+    });
   });
-}
+};
 
 // 获取设备列表
-const getApiDeviceList = async function (obj) {
+const getApiDeviceList = async function(obj) {
   // 获取设备列表
-  let res1 = await api.httpPost('http://flow.dev.redsoul.cn/app/device/getDeviceList', obj);
+  let res1 = await api.httpPost(
+    'http://flow.dev.redsoul.cn/app/device/getDeviceList',
+    obj
+  );
   // 设置信息数组
   let deviceArrayLength = res1.data.pageRows.length;
   if (deviceArrayLength === 0) {
@@ -50,7 +59,7 @@ const getApiDeviceList = async function (obj) {
     that.isFalse = false;
     let deviceArray = res1.data.pageRows;
     for (let i = 0; i < deviceArray.length; i++) {
-      let deviceObj = {}
+      let deviceObj = {};
       deviceObj.deviceId = deviceArray[i].deviceMac;
       deviceObj.deviceName = deviceArray[i].deviceName;
       deviceObj.deviceAddress = deviceArray[i].deviceAddress;
@@ -60,12 +69,9 @@ const getApiDeviceList = async function (obj) {
     }
     that.$apply();
   }
- 
-}
-
-
+};
 
 export default {
   httpPost,
   showModal
-}
+};
